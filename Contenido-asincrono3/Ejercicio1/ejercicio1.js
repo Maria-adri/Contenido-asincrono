fd8da614c2671bf02d76dba0aa1c1bc2d0066b72
Maria-adri <maria.rodri.adri@gmail.com>
2024-12-12 19:08:40 +0100

    12/12/24

--- Contenido-asincrono3/Ejercicio1/ejercicio1.js
+++ Contenido-asincrono3/Ejercicio1/ejercicio1.js
@@ -1,13 +1,16 @@
-const bloque = document.querySelectorAll('.bloque')
-const h2 = document.querySelectorAll('.h2')
+const bloques = document.querySelectorAll('.bloque');
+const titulos = document.querySelectorAll('.h2');
 
-h2.forEach( ( cadaH2 , i)=> {
-    h2[i].addEventListener('click', ()=>{
+titulos.forEach((titulo, i) => {
+    titulo.addEventListener('click', () => {
+        
+        const bloque = bloques[i];
+        const esActivo = bloque.classList.contains('activo');
 
-        bloque.forEach((cadaBloque , i )=>{
-            bloque[i].classList.remove('activo')
-        })
-        bloque[i].classList.add('activo')
+        bloques.forEach(b => b.classList.remove('activo'));
 
-    })
-})
+        if (!esActivo) {
+            bloque.classList.add('activo');
+        }
+    });
+});
