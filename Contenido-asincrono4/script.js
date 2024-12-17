85ac7ab19f17b0205c06de4ff4d20251c7580b5a
Maria-adri <maria.rodri.adri@gmail.com>
2024-12-17 13:31:39 +0100

    17/12/24

--- Contenido-asincrono4/script.js
+++ Contenido-asincrono4/script.js
@@ -1,19 +1,34 @@
-// script.js
 document.addEventListener("DOMContentLoaded", () => {
     const listItems = document.querySelectorAll(".brand-list li");
-    const images = document.querySelectorAll(".brand-display .image");
+    const display = document.querySelector(".brand-display");
+
+    const images = {
+        img1: "Fotos/jaguar.jpg",
+        img2: "Fotos/ferrari.avif",
+        img3: "Fotos/lamborghini1.webp",
+        img4: "Fotos/bmw2.webp",
+        img5: "Fotos/audi3.jpeg",
+        img6: "Fotos/tesla1.jpg",
+        img7: "Fotos/mercedes.jpg"
+    };
 
     listItems.forEach(item => {
-        item.addEventListener("click", () => {
-            // Resaltar texto activo
-            listItems.forEach(el => el.classList.remove("active"));
+        item.addEventListener("mouseover", () => {
+         
             listItems.forEach(el => el.classList.add("inactive"));
+            item.classList.remove("inactive");
             item.classList.add("active");
 
-            // Mostrar imagen correspondiente
+          
             const imageId = item.getAttribute("data-image");
-            images.forEach(img => img.classList.remove("active"));
-            document.getElementById(imageId).classList.add("active");
+            display.style.backgroundImage = `url(${images[imageId]})`;
+            display.style.opacity = "1"; 
+        });
+
+        item.addEventListener("mouseout", () => {
+          
+            listItems.forEach(el => el.classList.remove("active", "inactive"));
+            display.style.opacity = "0"; 
         });
     });
 });
